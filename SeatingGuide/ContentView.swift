29d4159c2d76b921321cf1cefcd06426d1e00e2c
Igor Malyarov <photo.igor@mac.com>
2019-06-25 23:05:16 +0300

    Initial Commit

---
+++ SeatingGuide/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SeatingGuide
+//
+//  Created by Igor Malyarov on 25.06.2019.
+//  Copyright © 2019 Igor Malyarov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
